27fc2f70c350d7817dd9677075cb5890e1551f85
Celeste Thomas <celestesmthomas@gmail.com>
2020-04-21 02:01:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,10 +10,10 @@ end
 def add_element_to_end_of_array(array, element)
   array = []
   array << element
-  
 end
 
 def add_element_to_start_of_array(array, element)
+  array.unshift(element)
   
 end
 
